fbf6cc6072e39c7333a74a9994a24e41798de0b5
Shubham Chaubey <shubhamchaubey2211@gmail.com>
2022-12-13 00:59:21 +0530

    Completed the Navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Navbar from './components/layout/Navbar';
+
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </div>
+    <Router>
+      <div className='flex flex-col justify-between h-screen'>
+      <Navbar />
+      <main>
+        Content
+      </main>
+      </div>
+    </Router>
   );
 }
 
