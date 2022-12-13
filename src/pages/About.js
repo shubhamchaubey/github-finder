e0231a7cf2166a4d92e63b88700a8ea4b760f9ff
Shubham Chaubey <shubhamchaubey2211@gmail.com>
2022-12-13 20:04:13 +0530

    COmpleted the page structure and completed the NotFound page

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+function About() {
+  return (
+    <div>
+      <h1 className="text-6xl mb-4"> GitHub Finder</h1>
+      <p>
+        This is the about part of my project that I would like to complete later
+        on. For now, just think of a huge text copy available here explaining
+        everything about this project.
+      </p>
+    </div>
+  );
+}
+
+export default About;
